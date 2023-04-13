9fbd9dda8997a7befedac54370ab4fc726e53642
Param Pal Singh <singhparampal97@gmail.com>
2023-04-13 15:13:57 +0200

    initial terraform setup

---
+++ terraform/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+    }
+  }
+  required_version = ">= 1.3.1"
+}
+
+provider "azurerm" {
+  features {}
+}
+
